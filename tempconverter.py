d675f8aa706eb00553c87a6512343ee0ab8ce651
NXS-DEV <105024951+NXS-DEV@users.noreply.github.com>
2022-11-21 13:01:50 -0500

    Update tempconverter.py
    The program can now re-ask the user each time until the user close the program.
    The program will not close anymore after one convertion.

--- tempconverter.py
+++ tempconverter.py
@@ -1,29 +1,26 @@
 # Temperature converter Celsius into F and F into Celsius.
 # formula of conversion
 # T(°Fahrenheit)=(T(°Celsius)*9/5)+32
-version = "0.0.0.2"
+version = "0.0.0.3"
 print("Welcome on the Temp Converter \nDeveloped by : Noxious")
 print("Temp Converter version: " + version)
-print("Option 1 - Convert from Celsius to Fahrenheit")
-print("Option 2 - Convert from Fahrenheit to Celsius")
-option = input("Choose an option - 1 or 2:")
-
-if option == "1":
-    try:
-        C = input("Type your temperature in Celsius: ")
-        F = (float(C) * 9 / 5) + 32
-        print("Temperature: " + str(F) + " degrees Fahrenheit")
-    except ValueError:
-        print("This is not an number , try again !")
-elif option == "2":
-    try:
-        F = input("Type your temperature in Fahrenheit: ")
-        C = (float(F) - 32) * 5 / 9
-        print("Temperature: " + str(C) + " degrees Celsius")
-    except ValueError:
-        print("This is not an number , try again !")
-elif option != "1" and "2":
-    print("Invalid option !")
-
-else:
-    print("Invalid option !")
+while True:
+    print("Option 1 - Convert from Celsius to Fahrenheit")
+    print("Option 2 - Convert from Fahrenheit to Celsius")
+    option = input("Choose an option - 1 or 2:")
+    if option == "1":
+        try:
+            C = input("Type your temperature in Celsius: ")
+            F = (float(C) * 9 / 5) + 32
+            print("Temperature: " + str(F) + " degrees Fahrenheit")
+        except ValueError:
+            print("This is not an number , try again !")
+    elif option == "2":
+        try:
+            F = input("Type your temperature in Fahrenheit: ")
+            C = (float(F) - 32) * 5 / 9
+            print("Temperature: " + str(C) + " degrees Celsius")
+        except ValueError:
+            print("This is not an number , try again !")
+    else:
+        print("Invalid option !")
